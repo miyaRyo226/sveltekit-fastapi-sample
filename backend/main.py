1be336f170fb4b731825f975dd6c79b236c7e300
ryoya <ryoya.miyamoto226@gmail.com>
2023-08-18 12:47:07 +0000

    fix: migration作成

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from prisma import Prisma
 
 app = FastAPI()
-
+prisma = Prisma()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.on_event("startup")
+async def startup():
+        await prisma.connect()
+        
+        
+@app.on_event("shutdown")
+async def shutdown():
+    await prisma.disconnect()
